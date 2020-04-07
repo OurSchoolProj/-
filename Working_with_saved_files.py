48b3c5628367275be2a7eeff1f1e6bebe0695c7d
arturabgaryan <ar12.abgaryan@gmail.com>
2020-04-07 17:40:43 +0300

    Last commit Calculator

--- Working_with_saved_files.py
+++ Working_with_saved_files.py
@@ -5,9 +5,10 @@ import tkinter
 import copy
 
 # -------------------------------------------------------------------------------------------------------
-op_dict = {'Черепаха': Turtle(), 'Вычислитель': Calculator(), 'Чертежник': Blueprinter()}
+op_dict = {'Черепаха': Turtle(), 'Вычислитель': Calculator(0, 1), 'Чертежник': Blueprinter()}
 main = tkinter.Tk()
-canvas = tkinter.Canvas(height=800, width=800, bg='white')
+canvas = tkinter.Canvas(height=800, width=800, bg='blue')
+variables = {}
 
 
 # ---------------------------------------------------------------------------------------------------
@@ -55,9 +56,15 @@ def complicated_argument(s):
     i = 0
     while s1[i] != ',':
         i += 1
-    res.append(eval(s1[:i]))
+    try:
+        res.append(eval(s1[:i]))
+    except Exception:
+        res.append(s1[:i])
     i += 1
-    res.append(eval(s1[i:]))
+    try:
+        res.append(eval(s1[i:]))
+    except Exception:
+        res.append(s1[i:])
     return res
 
 
@@ -82,6 +89,17 @@ def cycle_body(l):
     return res
 
 
+def f(s):
+    global variables
+    s1 = ''
+    for i in range(len(s)):
+        if s[i] in variables:
+            s1 += str(variables[s[i]])
+        else:
+            s1 += s[i]
+    return eval(s1)
+
+
 # -------------------------------------------------------------------------------------------------------
 def xs(x):
     return x + 400
@@ -94,11 +112,11 @@ def ys(y):
 def coords():
     global canvas
     for i in range(1, 40):
-        canvas.create_line(i * 20, 0, i * 20, 800, fill='black')
+        canvas.create_line(i * 20, 0, i * 20, 800, fill='white')
     for j in range(0, 820, 20):
-        canvas.create_line(0, j, 800, j)
-    canvas.create_line(400, 0, 400, 800, fill='purple')
-    canvas.create_line(0, 400, 800, 400, fill='purple')
+        canvas.create_line(0, j, 800, j, fill='white')
+    canvas.create_line(400, 0, 400, 800, fill='red', width=3)
+    canvas.create_line(0, 400, 800, 400, fill='red', width=3)
 
 
 # -------------------------------------------------------------------------------------------------------
@@ -112,11 +130,12 @@ def compiling_txt(file_name):
     f = False
     for i in range(1, len(l)):
         t = formatted_command(l[i])
-        if (t in op_dict[operator].command_list) or ('нц' in t) or ('пока' in t) or ('кц' in t):
+        if (t in op_dict[operator].command_list) or ('нц' in t) or ('пока' in t) or ('кц' in t) or ('=' in t) or (
+                'ввод' in t) or ('вывод' in t):
             f = True
         else:
             f = False
-            raise MySyntaxError('Синтаксическая ошибка {}'.format(t))
+            raise MySyntaxError('Синтаксическая ошибка в строке {}: {}'.format(i + 1, t))
     if f:
         op = l[0]
         if op == 'Чертежник':
@@ -124,7 +143,7 @@ def compiling_txt(file_name):
         elif op == 'Черепаха':
             op = Turtle()
         elif op == 'Вычислитель':
-            op = Calculator()
+            op = Calculator(0, 1)
         core_alg(l[1:], op)
         if isinstance(op, Turtle) or isinstance(op, Blueprinter):
             canvas.pack()
@@ -133,7 +152,6 @@ def compiling_txt(file_name):
 
 def core_alg(l, op):
     global canvas, main
-
     if isinstance(op, Blueprinter):
         coords()
         crd = op.pos()
@@ -146,7 +164,8 @@ def core_alg(l, op):
                 op.moveto(args[0], args[1])
                 point2 = copy.deepcopy(op.pos())
                 if op.f:
-                    canvas.create_line(xs(point1[0]), ys(point1[1]), xs(point2[0]), ys(point2[1]), fill='blue')
+                    canvas.create_line(xs(point1[0]), ys(point1[1]), xs(point2[0]), ys(point2[1]), fill='black',
+                                       width=2)
                 crd = op.pos()
 
             elif t == 'сместись_на_вектор':
@@ -155,7 +174,8 @@ def core_alg(l, op):
                 op.vector_move(args[0], args[1])
                 point2 = copy.deepcopy(op.pos())
                 if op.f:
-                    canvas.create_line(xs(point1[0]), ys(point1[1]), xs(point2[0]), ys(point2[1]), fill='blue')
+                    canvas.create_line(xs(point1[0]), ys(point1[1]), xs(point2[0]), ys(point2[1]), fill='black',
+                                       width=2)
                 crd = op.pos()
             elif t == 'подними_перо':
                 op.up()
@@ -205,7 +225,32 @@ def core_alg(l, op):
     elif isinstance(op, Calculator):
         i = 0
         while i < len(l):
-            pass
+            t = formatted_command(l[i])
+            if t == 'вывод':
+                a = function_argument(l[i])
+                if a in variables:
+                    print(variables[a])
+                else:
+                    print(a)
+            elif t == 'ввод':
+                a = function_argument(l[i])
+                variables[a] = int(input())
+            elif 'дробь' in t:
+                args = complicated_argument(l[i])
+                i1 = l[i].index('=')
+                k = l[i][:i1]
+                if args[0] in variables:
+                    args[0] = variables[args[0]]
+                if args[1] in variables:
+                    args[1] = variables[args[1]]
+                variables[k] = Calculator(args[0], args[1])
+            elif '=' in t:
+                i1 = l[i].index('=')
+                l1 = l[i][i1 + 1:]
+                t1 = f(l1)
+                variables[l[i][0]] = t1
+            i += 1
 
 
 compiling_txt('sample_file.txt')
+# print(complicated_argument('с=дробь(a, b)'))
